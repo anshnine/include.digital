3e2eb06b12854adf723579850988facba0a6c23d
romanenko <anshnine@gmail.com>
2021-09-07 02:00:28 +0500

    footer_ready

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ function displaylogo() {
 
     if ($(window).width() < 1200) {
         $("#footer_logo").attr("src", "/img/footer_logo_sm.svg"); // small logo
+        $("#header_logo").attr("src", "/img/header_sm.svg"); // small logo
     } else {
+        $("#header_logo").attr("src", "/img/header__logo.svg"); // big logo
         $("#footer_logo").attr("src", "/img/footer__logo.svg"); //big logo
     }
 
@@ -17,12 +19,14 @@ function displaylogo() {
     if ($(window).width() < 768) {
         $("#law").addClass('d-none');
     } else $("#law").removeClass('d-none ');
+
     if ($(window).width() < 576) {
+        $("#services").addClass('d-none');
         if ($("#li_logo").length === 0) {
             $("#contacts").addClass('text-center').append('<img id="li_logo" src="/img/footer_logo_sm.svg" alt="">');
         } // small logo
     } else {
-
+        $("#services").removeClass('d-none');
         $("#li_logo").detach();
     }
 }
